a76216cecea8a6c4e9427b8fe0dc37962666f1da
My Name <Xcastaneda@unal.edu.co>
2021-07-25 13:59:16 -0500

    Desarrollo taller.

---
+++ src/main/java/futbol/Futbolista.java
@@ -0,0 +1,65 @@
+package futbol;
+
+import java.lang.Comparable;
+
+public abstract class Futbolista implements Comparable<Object>{
+	private String nombre;
+	private int edad;
+	private final String posicion;
+	
+	public int compareTo(Object o) {
+		return this.compareTo(o);
+	}
+	
+	public Futbolista(){
+		this("Maradona",30, "Delantero");
+	}
+	
+	public Futbolista(String nombre,int edad,String posicion){
+		this.nombre = nombre;
+		this.edad = edad;
+		this.posicion = posicion;
+	}
+	
+	
+	
+	
+	
+	public String toString() {
+		return "El futbolista " + nombre +
+				" tiene " + edad + " y juega de " +
+				posicion;
+	}
+
+	public boolean equals(Futbolista f) {
+		boolean afirmacion = true;
+		if(compareTo(this) != 0) {
+			afirmacion = false;
+		}
+		return afirmacion;
+	}
+	
+	public abstract boolean jugarConLasManos();
+	
+	public void setNombre(String nom) {
+		nombre = nom;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+	
+	public String getPosicion() {
+		return posicion;
+	}
+	
+	
+}
