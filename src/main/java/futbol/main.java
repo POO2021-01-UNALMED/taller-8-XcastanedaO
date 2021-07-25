286bb3f7f3c6099dfc56d53648805a6a71f1eb02
My Name <Xcastaneda@unal.edu.co>
2021-07-25 14:44:27 -0500

    Update main.java

--- src/main/java/futbol/main.java
+++ src/main/java/futbol/main.java
@@ -1,7 +1,7 @@
 package futbol;
 
 public class main {
-	Futbolista f = new Jugador("Alejo", 20, "Delantero");
+	//Futbolista f = new Jugador("Alejo", 20, "Delantero");
 	Futbolista f1 = new Futbolista();
 	Futbolista f2 = new Futbolista();
 	
